44194bdc1bdc10fd400640b58f21a67baea2a5de
sebas <se.chacon@estudiantec.cr>
2024-10-21 15:01:19 -0600

    Connexión con servidor y IniciarSesión y Pantalla inicio Actualizado

--- InciarSesion.py
+++ InciarSesion.py
@@ -1,14 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from datetime import timedelta
-
+from connection import get_connection
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Cambia esto por una clave más segura en producción
-
-# Datos de prueba de usuarios
-usuarios_validos = {
-    'admin': '1234',
-    'usuario': '5678'
-}
 
 @app.route('/')
 def form():
@@ -16,42 +10,70 @@ def form():
 
 @app.route('/login', methods=['POST'])
 def login():
-    # Obtener los datos del formulario
+
+    # DATOS DE LA APLICACION
     usuario = request.form['usuario']
     contrasena = request.form['contrasena']
-    recordarme = request.form.get('recordarme')  # Capturamos el valor del checkbox "Recordarme"
-    
-    # Verificar las credenciales
-    if usuario in usuarios_validos and usuarios_validos[usuario] == contrasena:
-        # Guardar el usuario en la sesión
-        session['usuario'] = usuario
-        
-        # Si el checkbox "Recordarme" está marcado, ajustar la duración de la sesión
-        if recordarme:
-            session.permanent = True  # Esto hace que la sesión sea permanente
-            app.permanent_session_lifetime = timedelta(days=7)  # La sesión durará 7 días si se selecciona "Recordarme"
+    recordarme = request.form.get('recordarme')
+
+    try:
+        # CONEXION BD: RentACar
+        connection = get_connection()
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT contrasena FROM Usuarios WHERE nombre = ?", (usuario,))
+        result = cursor.fetchone()
+
+        # VERIFICACION DE LOGIN
+        if result and result[0] == contrasena:
+            session['usuario'] = usuario
+            # RECORDAR USUARIO
+            if recordarme:
+                session.permanent = True
+                app.permanent_session_lifetime = timedelta(days=7)
+            else:
+                session.permanent = False
+            # PANTALLA PRINCIPAL
+            return redirect(url_for('Inicio'))
         else:
-            session.permanent = False  # La sesión terminará al cerrar el navegador
+            flash('Credenciales incorrectas. Inténtelo de nuevo.')
+            return redirect(url_for('form'))
 
-        # Redirigir a la pantalla de inicio de vehículos
-        return redirect(url_for('Inicio'))  # Aquí te aseguras de que la redirección es a la página correcta
-    else:
-        flash('Credenciales incorrectas. Inténtelo de nuevo.')
+    except Exception as e:
+        flash(str(e))
         return redirect(url_for('form'))
+    finally:
+        connection.close()
 
+# Ruta para la pantalla principal (Inicio)
 @app.route('/Pantalla')
 def Inicio():
     if 'usuario' in session:
-        return render_template('Inicio.html', usuario=session['usuario'])  # Renderiza la pantalla de Inicio
+        try:
+            # CONEXION A BD RENTACAR
+            connection = get_connection()
+            cursor = connection.cursor()
+            cursor.execute("EXEC dbo.ListarVehiculos")  # STORED PROCEDURE
+            vehiculos = cursor.fetchall()
+
+            return render_template('Inicio.html', vehiculos=vehiculos)
+        except Exception as e:
+            flash(str(e))
+            return redirect(url_for('form'))
+        finally:
+            connection.close()
     else:
         flash('Por favor, inicie sesión primero.')
-        return redirect(url_for('form'))  # Si no ha iniciado sesión, redirige a la página de login
+        return redirect(url_for('form'))
 
-# Ruta para la pantalla de "Ingresar Vehículo"
+# RUTAS DE INICIO
 @app.route('/ingresar')
 def IngresarVehiculo():
-    return render_template('IngresarVehiculo.html')
+    return render_template('Inicio.html', usuario=session['usuario'])
 
+@app.route('/reportes')
+def reportes():
+    return render_template('reportes.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ connection.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+def get_connection():
+    try:
+        connection = pyodbc.connect(
+            #AQUI VAN LOS DATOS DEL SERVIDOR (EN ESTE CASO ES UN SERVIDOR LOCAL)
+            'DRIVER={SQL Server};'
+            'SERVER=CHACO-LAPTOP\SQLEXPRESS;'
+            'DATABASE=RentACar;'
+            'Trusted_Connection=yes;'
+        )
+        print("Conexión exitosa a la base de datos.")
+        return connection
+    except Exception as e:
+        print("Error: No se pudo conectar a la Base de Datos", e)
+        return None
